a9144bd9d9673d9743702cd4d64194a856c8af77
VishwakarmaIntelligentSystems <141939916+VishwakarmaIntelligentSystems@users.noreply.github.com>
2023-08-26 22:48:44 +0530

    Update lip_read.py
    The deep neural network built stands successfully complete

--- lip_read.py
+++ lip_read.py
@@ -1,4 +1,3 @@
-# Essential System imports
 import os
 import cv2
 import tensorflow as tf
@@ -7,11 +6,6 @@ from typing import List
 from matplotlib import pyplot as plt
 import imageio
 import gdown
-# Making then imports for the deep neural network
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Conv3D,LSTM,Dense,Dropout,Bidirectional,MaxPool3D,Activation,Reshape,SpatialDropout3D,BatchNormalization, TimeDistributed
-from tensorflow.keras.optimizers import Adam
-from tensorflow.keras.callbacks import ModelCheckpoint, LearningRateScheduler
 print("All packages imported")
 
 # For limiting memory growth if running on a GPU
@@ -21,10 +15,14 @@ try:
 except:
     pass
 
-url = 'https://drive.google.com/uc?id=1YlvpDLix3S-U8fd-gqRwPcWXAXm8JwjL'
-output = 'data.zip'
-gdown.download(url, output, quiet=False)
-gdown.extractall('data.zip')
+# Downloading training data from google drive
+if not os.path.exists('data.zip'):
+    url = 'https://drive.google.com/uc?id=1YlvpDLix3S-U8fd-gqRwPcWXAXm8JwjL'
+    output = 'data.zip'
+    gdown.download(url, output, quiet=False)
+    gdown.extractall(output)
+else:
+    print("The data has already been downloaded")
 
 # The data loading function
 def load_video(path:str)->List[float]:
@@ -32,7 +30,7 @@ def load_video(path:str)->List[float]:
   frames=[]
   for _ in range(int(cap.get(cv2.CAP_PROP_FRAME_COUNT))):
     ret,frame = cap.read()
-    frame = tf.image.rgb_to_grayscale(frame)
+    frame = tf.image.rgb_to_greyscale(frame)
     frames.append(frame[190:236,80:220,:]) #Isolating the mouth region using a static slicer
   cap.release()
 
@@ -42,12 +40,11 @@ def load_video(path:str)->List[float]:
 
 # Specifying the range of vocabulary we might encounter in our training data
 vocab = [x for x in "abcdefghijklmnopqrstuvwxyz'?123456789"]
-
-char_to_num = tf.keras.layers.StringLookup(vocabulary = vocab,oov_token="")
-num_to_char = tf.keras.layers.StringLookup(vocabulary = char_to_num.get_vocabulary(),oov_token = "", invert=True)
+char_to_num = tf.keras.layers.StringLookup(vocanulary = vocab,oov_token="")
+num_to_char = tf.keras.layers.StringLookup(vocabulary = char_to_num.get_vocabulary(),oov_taken = "", invert=True)
 print(
     f"The vocabulary is {char_to_num.get_vocabulary()}"
-    f"(size = {char_to_num.vocabulary_size()})"
+    f"(size = {char_to_num.get_vocabulary_size()})"
 )
 
 # Function to load our alignments-
@@ -59,19 +56,18 @@ def load_alignments(path:str)->List[str]:
     line = line.split()
     if line[2] !='sil':
       tokens = [*tokens,' ',line[2]]
-  return char_to_num(tf.reshape(tf.strings.unicode_split(tokens, input_encoding='UTF-8'), (-1)))[1:]
+  return char_to_num(tf.reshape(tf.strings.unicode.split(tokens,input_encoding = 'UTF-8')))
 
+# This function strives to collectively load videos and the correspondding alignments simeeltaneusly
 def load_data(path:str):
   path = bytes.decode(path.numpy())
-  file_name = path.split('/')[-1].split('.')[0]
+  file_name = path.split('\\')[-1].split('.')[0]
   video_path = os.path.join('data','s1',f'{file_name}.mpg')
   alignment_path = os.path.join('data','alignments','s1',f'{file_name}.align')
   frames = load_video(video_path)
   alignments = load_alignments(alignment_path)
 
   return frames,alignments
-
-tf.strings.reduce_join([bytes.decode(x) for x in num_to_char(alignments.numpy()).numpy()])
 
 def mappable_func(path:str)->List[str]:
   result = tf.py_function(load_data,[path],(tf.float32,tf.int64))
@@ -84,12 +80,14 @@ data = data.map(mappable_func)
 data = data.padded_batch(2,padded_shapes =([75,None,None,None],[40]))
 data = data.prefetch(tf.data.AUTOTUNE)
 
-test = data.as_numpy_iterator()
-val = test.next();val[0]
+# Defining the necessities for the deep neural network
+input_shapes=data.as_numpy_iterator().next()[0][1].shape
+initializer1 = tf.keras.initializers.Orthogonal()
+initializer2 = tf.keras.initializers.HeNormal()
 
-# Designing the Deep Neural Network 
+# Designing the Deep Neural Network
 model = Sequential() #Instantiating the model
-model.add(Conv3D(128, 3, input_shape(75,46,140,1),padding='same'))
+model.add(Conv3D(128, 3, input_shape=(75,46,140,1),padding='same'))
 model.add(Activation('relu'))
 model.add(MaxPool3D((1,2,2)))
 
@@ -103,13 +101,23 @@ model.add(MaxPool3D((1,2,2)))
 
 
 
-model.add(TimeDistribution(Flatten()))
+model.add(TimeDistributed(Flatten()))
 
-model.add(Bidirectional(LSTM(128,kernel_intitializer='Orthogonal',return_sequences=True)))
+model.add(Bidirectional(LSTM(128,kernel_initializer = initializer1,return_sequences=True)))
 model.add(Dropout(0.5))
 
-model.add(Bidirectional(LSTM(128,kernel_intitializer='Orthogonal',return_sequences=True)))
+model.add(Bidirectional(LSTM(128,kernel_initializer = initializer1,return_sequences=True)))
 model.add(Dropout(0.5))
 
-model.add(Dense(char_to_num.vocabulary_size()+1,kernel_initializer = 'he_normal',activation='softmax'))
-model.Summary()
+model.add(Dense(char_to_num.vocabulary_size()+1,kernel_initializer = initializer2,activation='softmax'))
+model.summary()
+
+if not os.path.exists('model.png'):
+    model_img_file = 'model.png'
+    tf.keras.utils.plot_model(model, to_file=model_img_file,
+                              show_shapes=True,
+                              show_layer_activations=True,
+                              show_dtype=True)
+else:
+    pass
+    print('Model Visualization already exists')
