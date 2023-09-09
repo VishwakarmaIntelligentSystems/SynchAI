16640e5c57b50a207eae9620abadcdd5b34977ee
VishwakarmaIntelligentSystems <141939916+VishwakarmaIntelligentSystems@users.noreply.github.com>
2023-09-09 13:38:39 +0530

    Update web_interface.py
    Final interface

--- web_interface.py
+++ web_interface.py
@@ -1,47 +1,57 @@
-import json
-import cv2
 from streamlit_webrtc import webrtc_streamer
 import streamlit as st
 import streamlit_lottie as st_lottie
-# import tensorflow as tf
-import mediapipe as mp
+import tensorflow as tf
 from apikey import api_key
 from langchain.llms import OpenAI
-from langchain.chains import SimpleSequentialChain,LLMChain
-from langchain.prompts import PromptTemplate
 import numpy as np
-import av
 import os
+from video_processing import VideoProcessor
+from modelutils import load_model
+from gen_utils import load_video
 
+#Basic page configuration
 st.set_page_config(
     page_title='SynchAI',
     page_icon='🤖',
 )
-
-facemesh = mp.solutions.face_mesh
-face = facemesh.FaceMesh(static_image_mode=True,min_detection_confidence=0.5,min_tracking_confidence=0.6)
-draw = mp.solutions.drawing_utils
 
 os.environ['OPENAI_API_KEY']=api_key
 llm = OpenAI(temperature=0.9)
 st.title('SynchAI')
 st.write('Aiding to track precision')
 
-class video_processing:
-    def process_video(self,frame):
-        rgb = frame.to_ndarray(format='rgb24')
-        output = face.process(rgb)
-        if output.multi_face_landmarks:
-            for i in output.multi_face_landmarks:
-                draw.draw_landmarks(rgb,i)
-        return av.VideoFrame.from_ndarray(rgb)
-webrtc_streamer(key='video',video_processor_factory=video_processing)
+# Setting up the streamer with all the pre-processing done!
+start_recording = st.button('Start')
+stop_recording = st.button('Stop')
 
+
+output_file = 'output.mp4'
+processor = VideoProcessor(output_file)
+
+class get_video:
+    def recv(self,frame):
+        return frame
+video_streamer=webrtc_streamer(key='Video',video_processor_factory=get_video)
+
+def get_next_frame_from_webrtc_stream(streamer):
+    if streamer:
+        frame = streamer.video_receiver.get_frame(timeout=10)
+        return frame
+
+while start_recording:
+    frame = get_next_frame_from_webrtc_stream(video_streamer)
+    if frame is not None:
+        processor.process_frame(frame)
+
+    if stop_recording:
+        processor.process_frame(frame)
+        break
+
+# processor.finish_processing()
 def generate_prompt(prompt):
     response = llm(prompt)
     st.info(response)
 
 if st.button('Generate Prompt'):
     generate_prompt(prompt = 'Generate a general interview question for the post of SDE')
-
-
